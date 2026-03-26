56cd3b5bb9088321509fa814142c5dc45993fd74
Eugene Gulyaev <gulyaeve@vk.com>
2026-03-26 17:55:35 +0300

    added parse mode

--- bot/app.py
+++ bot/app.py
@@ -1,9 +1,10 @@
 import logging
 
 from maxapi import Bot
+from maxapi.enums.parse_mode import ParseMode
 from bot.settings import settings
 
 logging.basicConfig(level=logging.INFO)
 
 
-bot = Bot(token=settings.MAX_API_TOKEN)
+bot = Bot(token=settings.MAX_API_TOKEN, parse_mode=ParseMode.HTML)
